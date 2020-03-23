f266a928348f3d4f8e44938653ff909bf8419b37
nontawatwuttikam <nontawatwuttikam@gmail.com>
2020-03-23 16:19:44 +0700

    Again

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask import jsonify
 # from covid19uncle import ThaiCovid19
 
-# appBlueprint = Blueprint("covid19",__name__)
+appBlueprint = Blueprint("covid19",__name__)
 
 # thai_result = ThaiCovid19()
 @appBlueprint.route('/')
