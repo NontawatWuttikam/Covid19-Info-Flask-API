9d90d4459a3cca057ea15f03114c50eb81211ab5
nontawatwuttikam <nontawatwuttikam@gmail.com>
2020-03-23 17:10:48 +0700

    Add Webhook endpoint

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 from flask import Blueprint
 from flask import jsonify
 from flask import Response
+from flask import request
 from covid19uncle import ThaiCovid19
 from covid19uncle import covid19
 import json
 
 appBlueprint = Blueprint("covid19",__name__)
-
 thai_result = ThaiCovid19()
+
 @appBlueprint.route('/')
 def landing():
     # print(thai_result)
@@ -21,6 +22,11 @@ def thai_inform():
     for i in range(len(key_list)):
         string = string + str(i) + ". " + key_list[i] + "\n"
     return Response(string,mimetype='text/json')
+
+@appBlueprint.route("/webhook", methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'POST':
+        return 'OK'
 
 @appBlueprint.route('/thai/all')
 def thai_inform_all():
