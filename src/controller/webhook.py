12180b4a972d9db953f57c89998b31cfc36ae239
nontawatwuttikam <nontawatwuttikam@gmail.com>
2020-03-25 13:40:04 +0700

    Add webhook message handler

--- src/controller/webhook.py
+++ src/controller/webhook.py
@@ -5,10 +5,21 @@ from flask import request
 from covid19uncle import ThaiCovid19
 from covid19uncle import covid19
 import json
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage,TextSendMessage
 
 appBlueprint = Blueprint("webhook",__name__)
+line_bot_api = LineBotApi('FMo5EGRDYJ2gg74JRPmR/G0QeaOaIg+DPEXWtz4bspWt7dvzwK61w7G+x4EkVLPdT0Lpvi4pNStIcvg3P//+9ujUoEO5uqurzm5kBVO6XUFHPQOEo0DH6MQr+Kux8odZwexl3GGJFXGv6zegH1j8DAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('9000b5a93374236a05ffa5683505f0ee')
 
 @appBlueprint.route("/webhook", methods=['GET', 'POST'])
 def webhook():
     if request.method == 'POST':
         return 'OK'
+    
+@handler.add(MessageEvent, message=TextMessage)
+def handler_message(evt):
+    line_bot_api.reply_message(
+        evt.reply_token,
+        TextSendMessage(text=evt.message.text)
+    )
