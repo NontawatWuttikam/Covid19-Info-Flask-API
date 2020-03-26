971bc02b197fb6713a6b164a42143ffee0f28f07
nontawatwuttikam <nontawatwuttikam@gmail.com>
2020-03-26 17:15:27 +0700

    remove header and total keys from global result key

--- src/service/covid19global_service.py
+++ src/service/covid19global_service.py
@@ -12,7 +12,10 @@ key_list_thai = util.get_keys_list(global_result)
 def global_inform():
     global global_result
     global key_list_thai
-    string = util.get_keys_string(global_result,"รายชื่อประเทศทั้งหมด\nกรุณาพิมพ์ชื่อประเทศเพื่อเรียกดูข้อมูล")
+    new_result = global_result.copy()
+    new_result.pop('total')
+    new_result.pop('header')
+    string = util.get_keys_string(new_result,"รายชื่อประเทศทั้งหมด\nกรุณาพิมพ์ชื่อประเทศเพื่อเรียกดูข้อมูล")
     return string
 
 def global_inform_country(country):
