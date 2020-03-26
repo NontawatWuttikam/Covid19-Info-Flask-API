2e56cd09dfe213e5c6ab11de7c958dad9feb2485
nontawatwuttikam <nontawatwuttikam@gmail.com>
2020-03-26 15:38:51 +0700

    Add Inquiry thai information into webhook handler

--- src/controller/webhook.py
+++ src/controller/webhook.py
@@ -2,11 +2,10 @@ from flask import Blueprint
 from flask import jsonify
 from flask import Response
 from flask import request
-from covid19uncle import ThaiCovid19
-from covid19uncle import covid19
 import json
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage,TextSendMessage
+from ..service import covid19thai_service
 
 appBlueprint = Blueprint("webhook",__name__)
 line_bot_api = LineBotApi('FMo5EGRDYJ2gg74JRPmR/G0QeaOaIg+DPEXWtz4bspWt7dvzwK61w7G+x4EkVLPdT0Lpvi4pNStIcvg3P//+9ujUoEO5uqurzm5kBVO6XUFHPQOEo0DH6MQr+Kux8odZwexl3GGJFXGv6zegH1j8DAdB04t89/1O/w1cDnyilFU=')
@@ -23,7 +22,8 @@ def webhook():
     
 @handler.add(MessageEvent, message=TextMessage)
 def handler_message(evt):
-    line_bot_api.reply_message(
-        evt.reply_token,
-        TextSendMessage(text=evt.message.text)
-    )
+    if "ไทย" in evt.message.text :
+        line_bot_api.reply_message(
+            evt.reply_token,
+            TextSendMessage(text=covid19thai_service.thai_inform_all())
+        )
